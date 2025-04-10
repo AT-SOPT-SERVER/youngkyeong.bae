a7a7590b4fd48fb99277447e0cacc7b9f47954ad
bykbyk0401 <bykbyk0401@gmail.com>
2025-04-10 12:26:50 +0900

    feat: ID 생성 책임 분리

--- src/main/java/org/sopt/service/PostService.java
+++ src/main/java/org/sopt/service/PostService.java
@@ -2,18 +2,18 @@ package org.sopt.service;
 
 import org.sopt.domain.Post;
 import org.sopt.repository.PostRepository;
+import org.sopt.util.IdGenerator;
 
 import java.util.List;
 
 public class PostService {
     private final PostRepository postRepository = new PostRepository();
-    private int postId = 1;
 
     public void createPost(String title) {
         if (postRepository.existsByTitle(title)) {
             throw new IllegalArgumentException("이미 존재하는 제목입니다.");
         }
-        Post post = new Post(postId++, title);
+        Post post = new Post(IdGenerator.generate(), title);
         postRepository.save(post);
     }
 
