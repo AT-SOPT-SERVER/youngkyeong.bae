5a34577cbf2eba2a01221725e0e9128b58aba127
bykbyk0401 <bykbyk0401@gmail.com>
2025-05-01 14:11:52 +0900

    feat: 회원가입 기능 구현

---
+++ src/main/java/org/sopt/service/UserService.java
@@ -0,0 +1,23 @@
+package org.sopt.service;
+
+import org.sopt.domain.User;
+import org.sopt.dto.request.UserCreateRequest;
+import org.sopt.exception.ConflictException;
+import org.sopt.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService (UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Long signup(UserCreateRequest userCreateRequest) {
+        userCreateRequest.validate();
+        if (userRepository.existsByName(userCreateRequest.name()))
+            throw new ConflictException("이미 존재하는 닉네임입니다.");
+        return userRepository.save(new User(userCreateRequest.name(), userCreateRequest.email())).getId();
+    }
+}
