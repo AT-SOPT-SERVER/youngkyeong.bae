da989535ccf8827cc3628da1c778039b05454f02
bykbyk0401 <bykbyk0401@gmail.com>
2025-05-01 12:47:19 +0900

    feat: 기본 생성 제약조건 설정

--- src/main/java/org/sopt/dto/request/PostRequest.java
+++ src/main/java/org/sopt/dto/request/PostRequest.java
@@ -1,3 +1,16 @@
-package org.sopt.dto;
+package org.sopt.dto.request;
 
-public record PostRequest(String title) {}
+import org.sopt.exception.BadRequestException;
+
+public record PostRequest(String title, String content) {
+    public void validate() {
+        if (title == null || title.isBlank())
+            throw new BadRequestException("제목은 비어 있을 수 없습니다.");
+        if (content == null || content.isBlank())
+            throw new BadRequestException("내용은 비어 있을 수 없습니다.");
+        if (title.length() > 30)
+            throw new BadRequestException("제목은 30자를 넘을 수 없습니다.");
+        if (content.length() > 1000)
+            throw new BadRequestException("내용은 1000자를 넘을 수 없습니다.");
+    }
+}
